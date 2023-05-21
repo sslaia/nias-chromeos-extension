521d0be4b9fcb5c5cc89e3edcb50cf8d9fa90f5b
sslaia <laia.sirus@gmail.com>
2023-05-21 21:43:54 +0100

    Updated to manifest v3

--- background.js
+++ background.js
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 var previousCharIsMagic = false;
 var contextID = -1;
 var lut = {"a": "ä", "A": "Ä", "o": "ö", "O":"Ö", "u":"ü", "U":"Ü", "w":"ŵ", "W":"Ŵ", "`": "`"};
